2bbd60147e034acf66c9299e2bd3dd0f4e382759
dodo102 <111326799+dodo102@users.noreply.github.com>
2024-06-12 16:38:36 +0900

    2

--- calendar/lib/component/schedule_bottom_sheet.dart
+++ calendar/lib/component/schedule_bottom_sheet.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:calendar/component/custom_text_field.dart';
+import 'package:flutter/services.dart';
 
 class ScheduleBottomSheeet extends StatefulWidget {
-  ScheduleBottomSheeet({Key? key}) : super(key: key);
+
+  const ScheduleBottomSheeet({Key? key}) : super(key: key);
 
   @override
   State<ScheduleBottomSheeet> createState() => _ScheduleBottomSheeetState();
@@ -15,6 +18,27 @@ class _ScheduleBottomSheeetState extends State<ScheduleBottomSheeet> {
       child: Container(
         height: MediaQuery.of(context).size.height / 2,
         color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(child: CustomTextField(
+                      label: '시작 시간',
+                      isTime: true,
+                    ),),
+                    const SizedBox(width: 16.0,),
+                    Expanded(child: CustomTextField(
+                      label: '종료 시간',
+                      isTime: true,
+                    ),),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
       ),
     );
   }
